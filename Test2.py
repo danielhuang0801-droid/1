3d92590b3a229d38a5db84c1c14621b1af1b7697
danielhuang0801-droid <danielhuang0801@gmail.com>
2026-09-21 13:42:40 +0800

    Create Test2.py

---
+++ Test2.py
@@ -0,0 +1,36 @@
+# test1.py
+import requests
+from bs4 import BeautifulSoup
+import time
+
+# 要
+stock = ["1101", "2330", "1102"]
+
+for i in range(len(stock)): 
+    stockid = stock[i]
+    
+    # 
+    url = "https://tw.stock.yahoo.com/quote/" + stockid + ".TW"
+    
+    #
+    r = requests.get(url)
+    
+    # 
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    #
+    price = soup.find('span', class_=["Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)", "Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c)", "Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)"]).getText()
+    
+    #
+    message = "股票 " + stockid + " 即時股價為 " + price
+    
+    #
+    token = 3qCvx0Kv8GUSg  # 替換成 BotFather 給的 Token
+    chat_id = 6275017364  # 替換成 userinfobot 給的 ID
+    
+  
+    api_url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+    requests.get(api_url)
+    
+    
+    time.sleep(3)
